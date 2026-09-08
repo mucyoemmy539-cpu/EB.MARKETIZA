d8a7a5b1a314f6d5e403f6228f881754b541d3d4
Mucyo Emmy <mucyoemmy539@gmail.com>
2026-09-08 16:00:46 +0200

    Update config.js
    Signed-off-by: Mucyo Emmy <mucyoemmy539@gmail.com>

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// E&B MARKETIZA - Supabase configuration
-
 window.EB_CONFIG = {
   SUPABASE_URL: "https://pniivrnhhghaalrmlquo.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_ptGTV8KdlaX9WZHqX1Z9dg_Y1Iwq9Vm"
